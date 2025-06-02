ca4c36dbd46eb3ccba396c1cfb7fb910dcd18d55
jo3rn <12082310+jo3rn@users.noreply.github.com>
2025-06-02 18:43:28 +0200

    Add semicolons

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import useTranslation from 'next-translate/useTranslation'
-import Trans from 'next-translate/Trans'
+import useTranslation from 'next-translate/useTranslation';
+import Trans from 'next-translate/Trans';
 import DateTimeElement from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
